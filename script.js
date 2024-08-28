5a66c1ce5611bbcc1d74f6311a6a77ef3ad1f10d
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2024-08-28 21:21:55 +0200

    Basic filtering functionality

---
+++ script.js
@@ -0,0 +1,39 @@
+const filterList = document.querySelector('.filter');
+const filterBtns = filterList.querySelectorAll('.filter-btn');
+const conferences = document.querySelectorAll('.conference');
+
+filterBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        const filter = e.currentTarget.getAttribute('data-filter');
+
+        // change the active btn
+        updateActiveBtn(e.currentTarget);
+
+        // filter the list
+        filterConferences(filter);
+    });
+});
+
+function updateActiveBtn(newActiveBtn) {
+    // find previously active btn
+    // & remove the active class from it
+    filterList.querySelector('.active').classList.remove('active');
+
+    // add active class to new active btn
+    newActiveBtn.classList.add('active');
+}
+
+function filterConferences(conferencesFilter) {
+    // get each conference
+    conferences.forEach(conf => {
+        const confCategory = conf.getAttribute('data-category');
+        
+        // check if that conference matches the filterConference
+        if (conferencesFilter === 'all' || conferencesFilter === confCategory) {
+            conf.removeAttribute('hidden');
+        } else {
+            conf.setAttribute('hidden', '');
+        }
+    });
+
+}
